5baf5fa851a5d597d1d33a37f847ba15be0a067d
Alex Garbero <aegarbero@gmail.com>
2024-05-04 11:23:48 -0700

    Initial commit

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,11 @@
+export default function SideBar() {
+    return (
+        <div>
+            <h2>The Brutal Martian Landscape</h2>
+            <div>
+                <p>Decription</p>
+                <p>kjsdhfjks</p>
+            </div>
+        </div>
+    )
+}
